30340f1c6cfbe2a212d44358406a17f75c8a71c8
MANTHRI NRUSIMHA TEJA SRI <136962154+M-N-TEJA-SRI@users.noreply.github.com>
2025-11-28 09:54:48 +0530

    Implement recursive function to print numbers from n to 1

---
+++ n_to_1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+void print(int n){
+    if(n>0){
+        cout<<n<<"\t";
+       print(n-1);
+    }
+}
+int main() {
+int n;
+cin>>n;
+print(n);
+    return 0;
+}
